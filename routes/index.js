99835e0cae7c64f606e449d98f10f5f742a654b9
steph <sjapodaca1103@gmail.com>
2022-03-11 20:13:09 -0800

    deleted all files to start fresh

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const api = require('./api');
-
-router.use('/api', api);
-router.use((req, res) => {
-    res.status(404).send('<h1>Wrong Route!</h1>');
-});
-
-module.exports = router;
